d01ad710dcd56c5c9f193a5717b5db7c72ad6922
Jack Tan <jacktan165@gmail.com>
2020-09-17 01:13:18 +1200

    feat: added POC for agenda.js

--- packages/@foodbudget/app/src/index.ts
+++ packages/@foodbudget/app/src/index.ts
@@ -1,17 +1,26 @@
 import puppeteer from "puppeteer";
 import { PrismaClient } from "@prisma/client";
+import Agenda from "agenda";
 
 (async () => {
   const prisma = new PrismaClient({ log: ["query"] });
   try {
-    await prisma.users.create({
-      data: {
-        username: "username2",
-        password: "password",
-        nickname: "test",
-      },
-    });
-    console.log("added");
+    // await prisma.users.create({
+    //   data: {
+    //     username: "username2",
+    //     password: "password",
+    //     nickname: "test",
+    //   },
+    // });
+    // console.log("added");
+
+    const mongoConnectionString = "mongodb://admin:pass@127.0.0.1:27017/admin";
+
+    const agenda = new Agenda({ db: { address: mongoConnectionString } });
+    agenda.define("test", async () => console.log("wee"));
+
+    await agenda.start();
+    await agenda.every("5 seconds", "test");
   } catch (err) {
     console.log(err);
   } finally {
