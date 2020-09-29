fce2cbe996772ab1e98beabcfc1844aa35dbc4a5
Jack Tan <jacktan165@gmail.com>
2020-09-29 13:50:24 +1300

    chore: made req optional

--- packages/@foodbudget/app/src/routes/recipes.ts
+++ packages/@foodbudget/app/src/routes/recipes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 const recipesRoutes = (app: Router): void => {
-  app.get('/recipes', (req, res) => res.status(200).send('Recipes'));
+  app.get('/recipes', (_req, res) => res.status(200).send('Recipes'));
 };
 
 export default recipesRoutes;
