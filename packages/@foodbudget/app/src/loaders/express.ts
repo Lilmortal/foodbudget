52608173884c6c9d14908a8e1a3878122c2ba312
Jack Tan <jacktan165@gmail.com>
2020-10-04 14:31:43 +1300

    chore: added Apollo Server POC (WIP)

--- packages/@foodbudget/app/src/loaders/express.ts
+++ packages/@foodbudget/app/src/loaders/express.ts
@@ -2,6 +2,7 @@ import {
   Express, Request, Response, NextFunction,
 } from 'express-serve-static-core';
 import { StatusError } from '@foodbudget/errors';
+import { ApolloServer, gql } from 'apollo-server-express';
 import { LoaderParams } from './loaders.type';
 import routes from '../routes';
 import { handleError } from '../utils/handleError';
@@ -26,9 +27,50 @@ const handleErrors = (app: Express) => {
   });
 };
 
+const typeDefs = gql`
+# This "Book" type defines the queryable fields for every book in our data source.
+type Book {
+  title: String
+  author: String,
+  hehe(num: Int!): String
+}
+
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "books" query returns an array of zero or more Books (defined above).
+type Query {
+  books: [Book],
+  test(num: Int!): String
+}
+
+type Mutation {
+  addBook(title: String, author: String): Book
+}`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+    test: () => 'lala',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 const expressLoader = ({ app, config, serviceManager }: LoaderParams): void => {
   handleHealthChecks(app);
-  app.use(config.api.prefix, routes(serviceManager));
+  routes(serviceManager);
+  server.applyMiddleware({ app, path: config.api.prefix });
 
   handle404Routes(app);
   handleErrors(app);
