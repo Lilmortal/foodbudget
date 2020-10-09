44fb028b4878317031c9f2f1e76c3a613fd87072
Jack Tan <jacktan165@gmail.com>
2020-10-09 21:42:29 +1300

    chore: removed unused try catch around middleware

--- packages/@foodbudget/api/src/app.ts
+++ packages/@foodbudget/api/src/app.ts
@@ -3,16 +3,10 @@ import server from './apolloServer';
 import config from './config';
 import loaders from './loaders';
 import logger from './logger';
-import handleError from './utils/prettifyError';
 
 const app = express();
 
-try {
-  server.applyMiddleware({ app, path: config.api.prefix });
-} catch (err) {
-  console.log('hhh');
-  handleError(err);
-}
+server.applyMiddleware({ app, path: config.api.prefix });
 
 loaders({ app });
 
