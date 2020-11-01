3a2ea784b5e201a52623b9f7af94489406ce5d90
Jack Tan <jacktan165@gmail.com>
2020-11-01 14:35:50 +1300

    chore: fix vscode warnings and errors around linting

--- packages/@foodbudget/frontend/src/UserFragment.tsx
+++ packages/@foodbudget/frontend/src/UserFragment.tsx
@@ -1,10 +1,10 @@
 import { graphql } from 'react-relay';
 
 const UserFragment = graphql`
-    fragment UserFragment on user {
-        email
-        nickname
-    }
+  fragment UserFragment on user {
+    email
+    nickname
+  }
 `;
 
 export default UserFragment;
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   singleQuote: true,
-  trailingComma: true,
+  trailingComma: 'all',
   printWidth: 80,
 };
