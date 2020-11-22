09af90ec10e94289f94c9c491de1e56aca19c1d1
Jack Tan <jacktan165@gmail.com>
2020-11-22 16:41:01 +1300

    chore: removed requirement for terraform variablels

--- packages/@foodbudget/deploy/main.tf
+++ packages/@foodbudget/deploy/main.tf
@@ -1,20 +1,18 @@
 provider "aws" {
-    region = "ap-southeast-2"
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_key
+  region = "ap-southeast-2"
 }
 
 resource "aws_vpc" "foodbudget_vpc" {
-    cidr_block = "10.10.0.0/16"
-    tags = {
-        Name = "foodbudget-vpc"
-    }
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "foodbudget-vpc"
+  }
 }
 
 // Internet gateway is to allow to send traffic out to the internet
 resource "aws_internet_gateway" "foodbudget_igw" {
-    vpc_id = aws_vpc.foodbudget_vpc.id
-    tags = {
-         Name = "foodbudget-igw"
-    }
+  vpc_id = aws_vpc.foodbudget_vpc.id
+  tags = {
+    Name = "foodbudget-igw"
+  }
 }
