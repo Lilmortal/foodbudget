8fb0c081ef8647bbed66fe608b64abd87886b6ee
Jack Tan <jacktan165@gmail.com>
2020-11-24 00:05:27 +1300

    ci: push api changes to ecr

--- packages/@foodbudget/deploy/userdata.sh
+++ packages/@foodbudget/deploy/userdata.sh
@@ -3,5 +3,5 @@ yum install -y docker
 service docker start
 aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 197204282783.dkr.ecr.ap-southeast-2.amazonaws.com >> /tmp/startup.log
 sudo usermod -aG docker ec2-user
-docker pull 197204282783.dkr.ecr.ap-southeast-2.amazonaws.com/foodbudget-repository:latest
-docker run -p 8080:8080 197204282783.dkr.ecr.ap-southeast-2.amazonaws.com/foodbudget-repository:latest
+docker pull 197204282783.dkr.ecr.ap-southeast-2.amazonaws.com/foodbudget-api:latest
+docker run -p 8080:8080 197204282783.dkr.ecr.ap-southeast-2.amazonaws.com/foodbudget-api:latest
